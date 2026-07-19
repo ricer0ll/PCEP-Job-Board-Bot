8df2d57036eca67c00661525dd5e587e9e75af9e
ricer0ll <brianle17360@gmail.com>
2026-07-19 00:44:05 -0700

    separated dto's

---
+++ discord-bot/api/workday/dto/greenhouse.go
@@ -0,0 +1,14 @@
+package dto
+
+type GreenhouseJobPosting struct {
+	JobTitle string `json:"job_title"`
+	Location string `json:"location"`
+}
+
+type GreenhouseJobPostingRequest struct {
+	URL string `json:"url"`
+}
+
+type GreenhouseJobPostingResponse struct {
+	Jobs []GreenhouseJobPosting `json:"jobs"`
+}
