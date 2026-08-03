d8362d7a883e38b75899ff35dbe32eaace963a4f
ricer0ll <brianle17360@gmail.com>
2026-08-03 14:28:05 -0700

    updated deploy script

--- deploy.sh
+++ deploy.sh
@@ -1,10 +1,13 @@
 #!/bin/bash
 
+echo "Stopping containers"
+docker compose down
+
 echo "Fetching latest changes from git"
 git pull
 
-echo "Stopping containers"
-docker compose down
+echo "Pulling any changes from Docker images"
+docker compose --profile prod pull
 
 echo "Rebuilding contianers"
 docker compose up -d --build
